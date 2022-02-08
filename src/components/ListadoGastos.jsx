ecc05be6b081e1b5cbe2f9be3a7baff830266148
amarismariano <mariano32josea@gmail.com>
2022-02-08 16:37:50 -0500

    Primer commit del proyecto

---
+++ src/components/ListadoGastos.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Gasto from "./Gasto";
+
+const ListadoGastos = ({
+  gastos,
+  setGastoEditar,
+  eliminarGasto,
+  filtro,
+  gastosFiltrados,
+}) => {
+  return (
+    <div className="listado-gastos contenedor">
+      {filtro ? (
+        <>
+          <h2>{gastosFiltrados.length ? "Gastos" : "No hay gastos aún"}</h2>
+          {gastosFiltrados.map((gasto) => (
+            <Gasto
+              id={gasto.id}
+              gasto={gasto}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2>
+            {gastos.length ? "Gastos" : "No hay gastos en esta categoría"}
+          </h2>
+          {gastos.map((gasto) => (
+            <Gasto
+              id={gasto.id}
+              gasto={gasto}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+            />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ListadoGastos;
